486bdc073b6028f91aedf7298708fca4485e868b
seo <seotownsend@icloud.com>
2014-11-09 18:13:45 -0500

    Working on quest.rb

--- lib/quest.rb
+++ lib/quest.rb
@@ -1,3 +1,44 @@
+#Contains a hash that maps quest_names onto another hash 
+#that contains quests names.  Lookup-table for class like
+#interface.
+#$quest_name.$action_name.block -> Stores the actual lambda expression of an action
+#$quest_name.$action_name.on -> Stores the list of events this action is sensetive to
+#{
+#  "my_quest" => {
+#    "my_action" => {
+#      :block => {
+#      },
+#      :on => [
+#        "my_event_a"
+#      ],
+#    }
+#  }
+#}
+$quests_hash = {}
+
+def action name, params={:on => []}, &block
+  raise "params must supply an on argument with an array of possible events" unless params[:on] and params[:on].class == Array
+  name = name.to_s
+  $quests_hash[@quest_name][name] = {}
+  $quests_hash[@quest_name][name][:on] = params[:on]
+  $quests_hash[@quest_name][name][:block] = block
+end
+
 def quest name
+  #Setup a new quest with this name
+  @quest_name = name.to_s
+  $quests_hash[@quest_name] = {}
+
+  #Add default overridable actions
+  ######################################################################
+  action "initialize" do
+    puts "Default Init Action"
+  end
+
+  action "finalize" do
+    puts "Default finialize action"
+  end
+  ######################################################################
+
   yield
 end
--- main.rb
+++ main.rb
@@ -1,8 +1,11 @@
 require File.expand_path("./config/init.rb", File.dirname(__FILE__))
 
-quest "my_quest" do
-
+quest :my_quest do
+  action :my_action, :on => ["my_situation"] do
+  end
 end
+
+p $quests_hash
 
 cron_event_loop_start_bg
 main_event_loop
